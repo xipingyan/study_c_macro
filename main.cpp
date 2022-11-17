e66e2636c39704e791d282f638d2ee4cd241d3f9
Yan, Xiping <xiping.yan@intel.com>
2022-11-17 10:39:50 +0800

    fix windows compile error

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv) {
   // test_method_2();
   // test_method_3();
   // test_method_4();
-  // test_method_5();
+  test_method_5();
   test_method_6();
   return 0;
 }
--- methold6.cpp
+++ methold6.cpp
@@ -6,12 +6,19 @@
 
 #include <iostream>
 
+#define OV_PP_EXPAND(X) X
+
+// For Windows, OV_PP_FIRST_ARG need this wrap.
+#define OV_PP_FIRST_ARG(...) OV_PP_EXPAND(OV_PP_FIRST_ARG_(__VA_ARGS__, 0))
+#define OV_PP_FIRST_ARG_(...) OV_PP_EXPAND(OV_PP_FIRST_ARG_GET(__VA_ARGS__))
+#define OV_PP_FIRST_ARG_GET(val, ...) val
+
 #define OV_PP_CAT_(x, y) x##y
 #define OV_PP_CAT(x, y) OV_PP_CAT_(x, y)
 #define OV_OR_ARG_PLACEHOLDER_1 1,
 #define OV_OR_ARG_PLACEHOLDER_0
-#define OV_FIRST_ARG_GET(val, ...) val
-#define OV_OR_(arg1_or_junk, arg2_or_junk) OV_FIRST_ARG_GET(arg1_or_junk arg2_or_junk 0)
+
+#define OV_OR_(arg1_or_junk, arg2_or_junk) OV_PP_FIRST_ARG(arg1_or_junk arg2_or_junk 0)
 #define OV_OR_2(x, y) OV_OR_(OV_PP_CAT(OV_OR_ARG_PLACEHOLDER_, x), OV_PP_CAT(OV_OR_ARG_PLACEHOLDER_, y))
 #define OV_OR_3(x, y, z) OV_OR_2(OV_OR_2(x, y), z)
 
@@ -21,6 +28,15 @@ void test_method_6()
 #define PRINT(fun) std::cout << #fun << " = " << fun << std::endl;
 
   PRINT(OV_OR_3(0, 0, 0))
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+#pragma message("OV_OR_3(0, 0, 0) = " STR(OV_OR_3(0, 0, 0)))
+#pragma message("OV_OR_3(0, 1, 0) = " STR(OV_OR_3(0, 1, 0)))
+#pragma message("OV_OR_3(0, 0, 1) = " STR(OV_OR_3(0, 1, 0)))
+#pragma message("OV_OR_2(0, 1) = " STR(OV_OR_2(0, 1)))
+
   PRINT(OV_OR_3(0, 0, 1))
   PRINT(OV_OR_3(0, 1, 0))
   PRINT(OV_OR_3(0, 1, 1))
